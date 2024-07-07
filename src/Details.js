cb848da65137052c5a94487c0aa8d55be9d30fd5
Prakash1256 <158434598+Prakash1256@users.noreply.github.com>
2024-07-07 14:30:08 +0530

    final Push

---
+++ src/Details.js
@@ -0,0 +1,141 @@
+
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Personal Details 
+export const personalDetails = {
+  name: "Kunwar Prakash Singh",
+  tagline: "I m a full-Stack devloper",
+  img: profile,
+  about: `Hello, I'm Kunwar Prakash Singh, a full stack developer skilled in HTML5, CSS3, MongoDB, SQL, JavaScript, React.js, and Node.js. I specialize in creating dynamic, responsive web applications with expertise in both front-end and back-end development.`,
+};
+
+//Social Media URLs 
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/",
+ 
+};
+
+//  Work Experience 
+export const workDetails = [
+  
+  {
+    Position: "FullStack-Devloper",
+    Company: `Masai`,
+    Location: "Bengaluru",
+    Type: "Training",
+    Duration: "Oct 2023 - Aug 2024",
+  },
+  
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "FullStack-Devloper",
+    Company: `Masai`,
+    Location: "Bengaluru",
+    Type: "Training",
+    Duration: "Oct 2023 - Aug 2024",
+  },
+  {
+    Position: "DIPLOMA IN ELEMENTARY EDUCATION (D.El.Ed.)",
+    Company: `Duja devi Mahavidyalya Sahtwar Ballia`,
+    Location: "Uttar-Pradesh",
+    Type: "Full Time",
+    Duration: "Jun 2019 - May 2024",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+//  Project Details 
+export const projectDetails = [
+  {
+    title: "Shine Heaven",
+    image: projectImage1,
+    description: `Shine Haven is a premium brand dedicated to providing high-quality personal care products. Our mission is to enhance your daily self-care routine with effective, gentle, and sustainable solutions that make you feel your best every day.`,
+    techstack: "HTML/CSS, JavaScript , React.js",
+    previewLink: "https://66713797ff903dc0919ec5ca--bucolic-banoffee-8c19f2.netlify.app/",
+    githubLink: "https://github.com/BabakhalilM/Huawei-Heroes_081",
+  },
+  {
+    title: "Decathlon",
+    image: projectImage2,
+    description: `Decathlon is a global leader in sports equipment and apparel, offering a wide range of high-quality, affordable products for over 70 sports. Our mission is to make the pleasure and benefits of sports accessible to everyone.From beginners to professional athletes... `,
+    techstack: "HTML/CSS, Bootstrap",
+    previewLink: "https://cw-project-git-main-sagar-prasads-projects.vercel.app/",
+    githubLink: "https://github.com/Sgrprsd11704088/CW-Project",
+  },
+  {
+    title: "Viatours",
+    image: projectImage3,
+    description: `Viator is a leading online platform for booking tours, activities, and experiences worldwide. With a vast selection of curated options, Viator helps travelers discover and book memorable experiences tailored to their interests and destinations.`,
+    techstack: "HTML/CSS, Figma",
+    previewLink: "https://cw-project-nlti-git-main-sagar-prasads-projects.vercel.app/",
+    githubLink: "https://github.com/Sgrprsd11704088/CW-Project",
+  },
+  {
+    title: "No-Broker",
+    image: projectImage4,
+    description: `NoBroker is a pioneering online platform that eliminates middlemen by connecting property owners directly with tenants and buyers. With a mission to make real estate transactions simpler and cost-effective, NoBroker offers a hassle-free solution for renting, buying, and selling properties without brokerage fees`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://660e695c7751b065e3797062--imaginative-pixie-6e83c9.netlify.app/",
+    githubLink: "https://github.com/Sartik0709/Compiler-Chintan-2345",
+  },
+ 
+];
+
+//  Contact Details 
+export const contactDetails = {
+  email: "ps4059269@gmail.com",
+  phone: "+91 7985139275",
+};
