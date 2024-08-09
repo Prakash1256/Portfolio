56cb53dd13eae9d5c609dc1a5d8ead813b142b14
Prakash1256 <158434598+Prakash1256@users.noreply.github.com>
2024-08-09 19:45:07 +0530

    push the code

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,9 @@
-
 // Logo images
 import logogradient from "./assets/logo.svg";
+
 // Profile Image
 import profile from "./assets/profile.png";
+
 // Tech stack images
 import html from "./assets/techstack/html.png";
 import css from "./assets/techstack/css.png";
@@ -14,11 +15,10 @@ import tailwind from "./assets/techstack/tailwind.png";
 import bootstrap from "./assets/techstack/bootstrap.png";
 import vscode from "./assets/techstack/vscode.png";
 import github from "./assets/techstack/github.png";
-
 import npm from "./assets/techstack/npm.png";
-
 import figma from "./assets/techstack/figma.png";
-// Porject Images
+
+// Project Images
 import projectImage1 from "./assets/projects/project1.png";
 import projectImage2 from "./assets/projects/project2.png";
 import projectImage3 from "./assets/projects/project3.png";
@@ -28,50 +28,53 @@ import projectImage6 from "./assets/projects/project6.png";
 
 // Logos
 export const logos = {
-  logogradient: logogradient,
-  
+  logogradient,
 };
 
 // Personal Details 
 export const personalDetails = {
   name: "Kunwar Prakash Singh",
-  tagline: "I m a full-Stack devloper",
+  tagline: "I'm a full-stack developer",
   img: profile,
   about: `Hello, I'm Kunwar Prakash Singh, a full stack developer skilled in HTML5, CSS3, MongoDB, SQL, JavaScript, React.js, and Node.js. I specialize in creating dynamic, responsive web applications with expertise in both front-end and back-end development.`,
 };
 
-//Social Media URLs 
+// Social Media URLs 
 export const socialMediaUrl = {
-  linkdein: "https://www.linkedin.com/in/kunwar-prakash-singh-7ba04b296",
+  linkedin: "https://www.linkedin.com/in/kunwar-prakash-singh-7ba04b296",
   github: "https://github.com/Prakash1256",
- 
 };
 
-//  Work Experience 
+// Work Experience 
 export const workDetails = [
-  
   {
-    Position: "FullStack-Devloper",
-    Company: `Masai`,
+    Position: "FullStack Developer",
+    Company: "Masai",
     Location: "Bengaluru",
     Type: "Training",
     Duration: "Oct 2023 - Aug 2024",
   },
-  
+  {
+    Position: "FullStack Developer Internship",
+    Company: "Edureka",
+    Location: "Bengaluru",
+    Type: "Training",
+    Duration: "july 2022 - january 2023",
+  },
 ];
 
-// Enter your Education Details here
+// Education Details
 export const eduDetails = [
   {
-    Position: "FullStack-Devloper",
-    Company: `Masai`,
+    Position: "FullStack Developer",
+    Company: "Masai",
     Location: "Bengaluru",
     Type: "Training",
     Duration: "Oct 2023 - Aug 2024",
   },
   {
     Position: "DIPLOMA IN ELEMENTARY EDUCATION (D.El.Ed.)",
-    Company: `Duja devi Mahavidyalya Sahtwar Ballia`,
+    Company: "Duja Devi Mahavidyalya Sahtwar Ballia",
     Location: "Uttar-Pradesh",
     Type: "Full Time",
     Duration: "Jun 2019 - May 2024",
@@ -80,35 +83,35 @@ export const eduDetails = [
 
 // Tech Stack and Tools
 export const techStackDetails = {
-  html: html,
-  css: css,
-  js: js,
-  react: react,
-  redux: redux,
-  sass: sass,
-  tailwind: tailwind,
-  bootstrap: bootstrap,
-  vscode: vscode,
-  npm: npm,
-  github: github,
-  figma: figma,
+  html,
+  css,
+  js,
+  react,
+  redux,
+  sass,
+  tailwind,
+  bootstrap,
+  vscode,
+  npm,
+  github,
+  figma,
 };
 
-//  Project Details 
+// Project Details 
 export const projectDetails = [
   {
     title: "Shine Heaven",
     image: projectImage1,
     description: `Shine Haven is a premium brand dedicated to providing high-quality personal care products. Our mission is to enhance your daily self-care routine with effective, gentle, and sustainable solutions that make you feel your best every day.`,
-    techstack: "HTML/CSS, JavaScript , React.js",
+    techstack: "HTML/CSS, JavaScript, React.js",
     previewLink: "https://66713797ff903dc0919ec5ca--bucolic-banoffee-8c19f2.netlify.app/",
     githubLink: "https://github.com/BabakhalilM/Huawei-Heroes_081",
   },
   {
-    title: "clone of better.com",
+    title: "Clone of better.com",
     image: projectImage5,
     description: `Better.com is a digital mortgage lender and financial services company that aims to simplify the home-buying process. Founded in 2016, Better.com leverages technology to provide a fast, transparent, and user-friendly experience for obtaining mortgages, refinancing, and managing home loans.`,
-    techstack: "HTML/CSS, JavaScript ,Next.js, Tailwind.css",
+    techstack: "HTML/CSS, JavaScript, Next.js, Tailwind.css",
     previewLink: "https://fluffy-lokum-2e9ded.netlify.app/",
     githubLink: "https://github.com/Prakash1256/better",
   },
@@ -116,15 +119,14 @@ export const projectDetails = [
     title: "Clone of Springdale School",
     image: projectImage6,
     description: `Springdale School Clone is a conceptual project aimed at replicating the functionalities and features of a school management system. It provides a comprehensive solution for managing various aspects of school operations, ensuring efficient and streamlined administration.`,
-    techstack: "HTML/CSS, JavaScript ,React.js, Tailwind.css",
+    techstack: "HTML/CSS, JavaScript, React.js, Tailwind.css",
     previewLink: "https://66a75d1a195bb1063d3bc4c8--dreamy-vacherin-367ff1.netlify.app/",
     githubLink: "https://github.com/Prakash1256/internship_project",
   },
-  
   {
     title: "Decathlon",
     image: projectImage2,
-    description: `Decathlon is a global leader in sports equipment and apparel, offering a wide range of high-quality, affordable products for over 70 sports. Our mission is to make the pleasure and benefits of sports accessible to everyone.From beginners to professional athletes... `,
+    description: `Decathlon is a global leader in sports equipment and apparel, offering a wide range of high-quality, affordable products for over 70 sports. Our mission is to make the pleasure and benefits of sports accessible to everyone. From beginners to professional athletes...`,
     techstack: "HTML/CSS, Bootstrap",
     previewLink: "https://cw-project-git-main-sagar-prasads-projects.vercel.app/",
     githubLink: "https://github.com/Sgrprsd11704088/CW-Project",
@@ -137,19 +139,17 @@ export const projectDetails = [
     previewLink: "https://cw-project-nlti-git-main-sagar-prasads-projects.vercel.app/",
     githubLink: "https://github.com/Sgrprsd11704088/CW-Project",
   },
-   {
+  {
     title: "No-Broker",
     image: projectImage4,
-    description: `NoBroker is a pioneering online platform that eliminates middlemen by connecting property owners directly with tenants and buyers. With a mission to make real estate transactions simpler and cost-effective, NoBroker offers a hassle-free solution for renting, buying, and selling properties without brokerage fees`,
+    description: `NoBroker is a pioneering online platform that eliminates middlemen by connecting property owners directly with tenants and buyers. With a mission to make real estate transactions simpler and cost-effective, NoBroker offers a hassle-free solution for renting, buying, and selling properties without brokerage fees.`,
     techstack: "HTML/CSS, JavaScript",
     previewLink: "https://660e695c7751b065e3797062--imaginative-pixie-6e83c9.netlify.app/",
     githubLink: "https://github.com/Sartik0709/Compiler-Chintan-2345",
   },
- 
- 
 ];
 
-//  Contact Details 
+// Contact Details 
 export const contactDetails = {
   email: "ps4059269@gmail.com",
   phone: "+91 7985139275",
